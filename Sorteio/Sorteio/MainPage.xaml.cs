6de8b085de0dc8eb55b1191902324bce8e8ba0c1
Fabricio Curvello <tiofab@gmail.com>
2018-06-28 22:02:04 -0300

    Lógica OK. Falta inserir as cartas. Imagens já no projeto

--- Sorteio/Sorteio/MainPage.xaml.cs
+++ Sorteio/Sorteio/MainPage.xaml.cs
@@ -9,9 +9,64 @@ namespace Sorteio
 {
 	public partial class MainPage : ContentPage
 	{
+        static int numeroSorteado;
+        static int score;
+
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+            numeroSorteado = sortear();
+            score = 0;
+            DisplayAlert("Sorteio", "Sorteado: " + (numeroSorteado + 1), "Ok");
+        }
+
+        private int sortear() {
+            Random rand = new Random();
+            int sorteado = rand.Next(3);//sorteia de 0 a 2
+            return sorteado;
+        }
+
+        private void BtnCarta1_Clicked(object sender, EventArgs e) {
+            if (numeroSorteado == 0) {
+                DisplayAlert("Carta 1", "GANHOU", "Ok");
+                score += 7;
+                LbScore.Text = "ACERTOU";
+            } else {
+                DisplayAlert("Carta 1", "PERDEU", "Ok");
+                score -= 5;
+            }
+            LbScore.Text = "Pontos: " + score;
+        }
+
+        private void BtnCarta2_Clicked(object sender, EventArgs e) {
+            if (numeroSorteado == 1) {
+                DisplayAlert("Carta 2", "GANHOU", "Ok");
+                score += 7;
+            } else {
+                DisplayAlert("Carta 2", "PERDEU", "Ok");
+                score -= 5;
+            }
+            LbScore.Text = "Pontos: " + score;
+        }
+
+        private void BtnCarta3_Clicked(object sender, EventArgs e) {
+            if (numeroSorteado == 2) {
+                DisplayAlert("Carta 3", "GANHOU", "Ok");
+                score += 7;
+            } else {
+                DisplayAlert("Carta 3", "PERDEU", "Ok");
+                score -= 5;
+            }
+            LbScore.Text = "Pontos: " + score;
+        }
+
+        private void BtnRestart_Clicked(object sender, EventArgs e) {
+            numeroSorteado = sortear();
+            score = 0;
+            LbScore.Text = "Pontos: " + score;
+            DisplayAlert("Sorteio", "Sorteado: " + (numeroSorteado + 1), "Ok");
+        }
+
+        
+    }
 }
---
+++ Sorteio/Sorteio/Modelo/Carta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorteio.Modelo
+{
+    class Carta
+    {
+
+    }
+}
